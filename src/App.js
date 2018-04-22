6b67838d76a22daa0ffd6a95a353db10f82c7f15
odisei369 <petrenko.ilia@gmail.com>
2018-04-22 20:45:28 +0200

    timer implemented

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import * as React from 'react';
 import Home from './view/Home';
 
 import './styles/theme.sass';
+import Counter from './view/Counter';
 
 class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  boom(){
+    alert("boom")
+  }
   render() {
     return (
       <main>
         <Home username="DaftCoder" />
+        <Counter from={123} to={128} onSuccess={this.boom}/>
       </main>
     );
   }
---
+++ src/view/Counter.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Counter extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {currentNumber: props.from,
+                      intervalWorking: true};
+        this.resetCounter = this.resetCounter.bind(this);
+    }
+
+    componentDidMount(){
+        this.bindInterval();
+    }
+
+     componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    bindInterval(){
+        this.setState({intervalWorking: true});
+        this.interval = setInterval(() => {
+            this.setState({currentNumber: this.state.currentNumber + 1});
+            if(this.state.currentNumber == this.props.to){
+                if(this.props.onSuccess){
+                    this.props.onSuccess();
+                }
+                clearInterval(this.interval);
+                this.setState({intervalWorking: false});
+            }
+          }, 1000);
+    }
+
+    resetCounter(){
+        if(this.state.currentNumber == this.props.to){
+            this.setState({currentNumber: this.props.from});
+            this.bindInterval();
+            return;
+        }
+        if(this.state.intervalWorking){
+            clearInterval(this.interval);
+            this.setState({intervalWorking: false});
+        } else {
+            this.bindInterval();
+        }
+
+    }
+
+    toMMSS (sec_num) {
+        var minutes = Math.floor(sec_num / 60);
+        var seconds = sec_num - (minutes * 60);
+
+        if (minutes < 10) {minutes = "0"+minutes;}
+        if (seconds < 10) {seconds = "0"+seconds;}
+        return minutes+':'+seconds;
+    }
+
+    render() {
+        const { currentNumber } = this.state;
+        const { to } = this.props;
+
+        let message;
+        if (this.state.intervalWorking){
+            message = null;
+        } else if(this.state.currentNumber == this.props.to){
+            message = <div>completed</div>;
+        } else {
+            message = <div>paused</div>;
+        }
+
+        return (
+        <div onClick={this.resetCounter} style={{border: "1px solid red"}}>
+            <div>Current timer state: {this.toMMSS(currentNumber)}</div>
+            <div>It will count until: {this.toMMSS(to)}</div>
+            {message}
+        </div>
+        );
+    }
+}
+
+Counter.propTypes = {
+    onSuccess: PropTypes.func,
+    from: PropTypes.number.isRequired,
+    to: PropTypes.number.isRequired
+};
+
+  export default Counter;
